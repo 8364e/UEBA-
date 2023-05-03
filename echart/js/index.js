30b7159596327bd9f22f8ac34f56008d6494aa5c
8364e <119795537+8364e@users.noreply.github.com>
2023-05-03 10:24:49 +0800

    Add files via upload

---
+++ echart/js/index.js
@@ -0,0 +1,519 @@
+
+
+function dialogBox(message) {
+  if (message) {
+    $('#winmsg').html(message);
+  }
+  // 显示遮罩和对话框
+  $('#winmsg').removeClass("hide");
+}
+
+//入口函数
+  //一. 1.2模块有tab栏切换
+  $('.monitor .tabs>a').on('click',function(){
+      $(this).addClass('active').siblings('a').removeClass('active');
+      //获取当前点击的页签的索引
+      let idx = $(this).index();
+      //找到索引一致的页面显示,其他的页面影藏
+      $('.monitor .content').eq(idx).show().siblings('.content').hide();
+  })
+  //自动的换
+  // let monitorIdx = 0;
+  // setInterval(function(){
+  //   monitorIdx++;
+  //   if (monitorIdx == 2) {
+  //     monitorIdx = 0;
+  //   }
+  //   //页签
+  //   $(".monitor .tabs>a")
+  //     .eq(monitorIdx)
+  //     .addClass("active")
+  //     .siblings("a")
+  //     .removeClass("active");
+  //   //页面
+  //   $(".monitor .content").eq(monitorIdx).show().siblings(".content").hide();
+  // }, 4000);
+
+  //二. 1.2模块有一个无限轮播效果
+  function lunbo() {
+    $(".monitor .carousel>ul").animate(
+      {
+        top: "-525px",
+      },
+      10000,
+      "linear",
+      function () {
+        $(".monitor .carousel>ul").css("top", 0);
+      }
+    );
+  }
+  lunbo();
+  setInterval(lunbo, 8000);
+
+(function () {
+  // 实例化对象
+  var myChart = echarts.init(document.querySelector(".bar .chart"));
+  // 指定配置和数据
+  var option = {
+    color: ["#2f89cf"],
+    tooltip: {//图标提示框组件
+      trigger: "axis",//触发方式：axis（坐标轴） item(柱状图，饼图)
+      axisPointer: {
+        // 坐标轴指示器，坐标轴触发有效
+        type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    grid: {//网格配置 控制图表大小
+      left: "0%",
+       top: "20%",
+      right: "2%",
+      bottom: "2%",
+      containLabel: true//显示y轴标签
+    },
+    xAxis: [//x轴
+      {
+        type: "category",//category相当于自变量 value相当于因变量
+        data: [
+          "第一季度",
+          "第二季度",
+          "第三季度",
+          "第四季度"
+        ],
+        axisTick: {
+          alignWithLabel: true
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: "12"
+          }
+        },
+        axisLine: {
+          show: false
+        }
+      }
+    ],
+    yAxis: [//y轴
+      {
+        type: "value",
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: "12"
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+            // width: 1,
+            // type: "solid"
+          }
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    //系列图标配置 决定显示哪种类型的图标
+    //bar是柱形图  line是折线图
+    //series有几部分则有几条折线或柱子
+    series: [
+      {
+        name: "直接访问",
+        type: "bar",
+        barWidth: "35%",
+        data: [500, 800, 1000, 1300, 1800, 1200, 600],
+        itemStyle: {
+          barBorderRadius: 5
+        }
+      }
+    ]
+  };
+
+  // 把配置给实例对象
+  myChart.setOption(option);
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+
+  // 数据变化
+  var dataAll = [
+    { year: "2022", data: [500, 800, 1000, 1300, 1800, 1200, 600] },
+    { year: "2023", data: [1000, 1400, 1350, 800, 1800, 1400, 700] }
+  ];
+
+  $(".bar h2 ").on("click", "a", function() {
+    option.series[0].data = dataAll[$(this).index()].data;
+    myChart.setOption(option);
+  });
+})();
+
+// 折线图定制
+(function() {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".line .chart"));
+
+  // (1)准备数据
+  var data = {
+    year: [
+      [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+      [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
+    ]
+  };
+
+  // 2. 指定配置和数据
+  var option = {
+    color: ["#00f2f1", "#ed3f35"],
+    tooltip: {
+      // 通过坐标轴来触发
+      trigger: "axis"
+    },
+    legend: {//图例组件
+      // 距离容器10%
+      right: "10%",
+      // 修饰图例文字的颜色
+      textStyle: {
+        color: "#4c9bfd"
+      }
+      // 如果series 里面设置了name，此时图例组件的data可以省略
+    },
+    grid: {
+      top: "20%",
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      show: true,
+      borderColor: "#012f4a",
+      containLabel: true
+    },
+
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: [
+        "1号",
+        "4号",
+        "7号",
+        "10号",
+        "13号",
+        "16号",
+        "19号",
+        "22号",
+        "25号",
+        "28号",
+        "31号"
+      ],
+      // 去除刻度
+      axisTick: {
+        show: false
+      },
+      // 修饰刻度标签的颜色
+      axisLabel: {
+        color: "rgba(255,255,255,.7)"
+      },
+      // 去除x坐标轴的颜色
+      axisLine: {
+        show: false
+      }
+    },
+    yAxis: {
+      type: "value",
+      // 去除刻度
+      axisTick: {
+        show: false
+      },
+      // 修饰刻度标签的颜色
+      axisLabel: {
+        color: "rgba(255,255,255,.7)"
+      },
+      // 修改y轴分割线的颜色
+      splitLine: {
+        lineStyle: {
+          color: "#012f4a"
+        }
+      }
+    },
+    series: [
+      {
+        name: "上月",
+        type: "line",
+        // 是否让线条圆滑显示
+        smooth: true,
+        data: data.year[0]
+      },
+      {
+        name: "该月",
+        type: "line",
+        smooth: true,
+        data: data.year[1]
+      }
+    ]
+  };
+  // 3. 把配置和数据给实例对象
+  myChart.setOption(option);
+
+  // 重新把配置好的新数据给实例对象
+  myChart.setOption(option);
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+})();
+
+// 折线图 优秀作品
+(function() {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".line2 .chart"));
+
+  option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        lineStyle: {
+          color: "#dddc6b"
+        }
+      }
+    },
+    legend: {
+      top: "0%",
+      textStyle: {
+        data: ["本月", "上月"],
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12"
+      }
+    },
+    //网格配置  图表大小
+    grid: {
+      left: "2%%",
+      top: "10%",
+      right: "3%",
+      bottom: "3%",
+      containLabel: true
+    },
+
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)"
+          }
+        },
+
+        data: [
+          "1号",
+          "4号",
+          "7号",
+          "10号",
+          "13号",
+          "16号",
+          "19号",
+          "22号",
+          "25号",
+          "28号",
+          "31号"
+        ]
+      },
+      {
+        axisPointer: { show: false },
+        axisLine: { show: false },
+        position: "bottom",
+        offset: 20
+      }
+    ],
+
+    yAxis: [
+      {
+        type: "value",
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "上月",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#0184d5",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(1, 132, 213, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(1, 132, 213, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#0184d5",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          60,
+          20,
+          40,
+          20,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          60,
+          20,
+          40,
+          20,
+          40,
+          30,
+          60,
+          20,
+          40,
+          20,
+          40
+        ]
+      },
+      {
+        name: "本月",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#00d887",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [
+          50,
+          30,
+          50,
+          60,
+          10,
+          50,
+          30,
+          50,
+          60,
+          40,
+          60,
+          40,
+          80,
+          30,
+          50,
+          60,
+          10,
+          50,
+          30,
+          70,
+          20,
+          50,
+          10,
+          40,
+          50,
+          30,
+          70,
+          20,
+          50,
+          10,
+          40
+        ]
+      }
+    ]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+})();
+
+
+  
